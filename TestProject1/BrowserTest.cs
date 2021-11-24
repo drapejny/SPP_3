aeeb97d6ab75126fdb4e153794ef9570d27291ad
Slizh Anton <antoshka-51762@yandex.by>
2021-11-24 11:31:44 +0300

    Add browser tests

---
+++ TestProject1/BrowserTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using AssemblyAnalyzer;
+
+namespace TestProject1
+{
+    public class BrowserTest
+    {
+
+        [Test]
+        public void LoadAssemblyTest()
+        {
+            string filePath = "D:\\VS Projects\\ABCD\\ABCD\\bin\\Debug\\net472\\ABCD.dll";
+            AssemblyBrowser browser = new AssemblyBrowser();
+            var actual = browser.GetAssemblyInfo(filePath);
+            Assert.IsNotNull(actual);
+        }
+
+        [Test]
+        public void BadFileNameLoadTest()
+        {
+            string filePath = "sdafasdf";
+            AssemblyBrowser browser = new AssemblyBrowser();
+            Assert.Throws<System.IO.FileNotFoundException>(() => browser.GetAssemblyInfo(filePath));
+        }
+
+    }
+}
